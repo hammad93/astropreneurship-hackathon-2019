afe806b8cc560d2b3e74cbf6f0f6f875929c07e5
hammad93 <ham.u@live.com>
2019-06-08 17:15:37 -0400

    Add magnetic level 1 to Cesium display

--- display.js
+++ display.js
@@ -2,3 +2,28 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate : true
 });
 viewer.dataSources.add(Cesium.CzmlDataSource.load('https://raw.githubusercontent.com/hammad93/astropreneurship-hackathon-2019/master/iss.czml'));
+var entities = viewer.entities;
+
+var i;
+for (i = 0; i < 3; ++i) {
+    entities.add({
+        position : Cesium.Cartesian3.fromDegrees(0, 0, 0),
+        ellipsoid : {
+            radii : new Cesium.Cartesian3(9000000.0, 9000000.0, 6500000.0),
+            outline : true,
+            outlineColor : Cesium.Color.WHITE,
+            outlineWidth : 2,
+            material : Cesium.Color.DARKGREY.withAlpha(0.1)
+        }
+    });
+    entities.add({
+        position : Cesium.Cartesian3.fromDegrees(180, 0, 0),
+        ellipsoid : {
+            radii : new Cesium.Cartesian3(9000000.0, 9000000.0, 6500000.0),
+            outline : true,
+            outlineColor : Cesium.Color.WHITE,
+            outlineWidth : 2,
+            material : Cesium.Color.DARKGREY.withAlpha(0.1)
+        }
+    });
+}
